9eb76221986e4a787d635317f69216fbf1cac8d5
OakenKnight <alexignjat1998@gmail.com>
2019-11-08 23:39:04 +0100

    #dodat mainframe, toolbar, dijalog za gasenje

---
+++ SSluzba/src/controller/EntityController.java
@@ -0,0 +1,28 @@
+package controller;
+
+
+public class EntityController {
+	
+	private static EntityController instance = null;
+	
+	public static EntityController getInstance() {
+		if (instance == null) {
+			instance = new EntityController();
+		}
+		return instance;
+	}
+	private EntityController() {}
+
+	public void addEntity() {
+		//TODO: preraditi metodu da dodaje entitete
+	}
+	public void editEntity() {
+		//TODO: preraditi metodu da menja entitete
+	}
+	public void removeEntity() {
+		//TODO: preraditi metodu da brise entitete
+	}
+	public void searchEntity() {
+		//TODO: preraditi metodu da trazi entitete
+	}
+}
