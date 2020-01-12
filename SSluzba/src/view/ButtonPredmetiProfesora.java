6d110c3a6b99edc68ce381b71b1ce2c815d82dc0
OakenKnight <alexignjat1998@gmail.com>
2020-01-12 02:21:53 +0100

    #dodato dugme za prikaz predmeta na profesoru, ne radi, ima bagova

---
+++ SSluzba/src/view/ButtonPredmetiProfesora.java
@@ -0,0 +1,92 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.EventObject;
+
+import javax.swing.AbstractCellEditor;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.event.CellEditorListener;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableCellRenderer;
+public class ButtonPredmetiProfesora extends AbstractCellEditor implements TableCellEditor,TableCellRenderer,MouseListener{
+
+
+	private JButton renderB;
+	private JButton editB;      
+	private JTable table; 
+	
+	public ButtonPredmetiProfesora(JTable t ,  int column) {
+		this.table = t;
+		this.table.getColumnModel().getColumn(column).setCellRenderer(this);
+		this.table.getColumnModel().getColumn(column).setCellEditor(this);
+		this.table.addMouseListener(this);
+
+		this.renderB = new JButton("Prikazi");
+		this.editB = new JButton("Prikazi");
+
+		
+	}
+	
+
+	@Override
+	public Object getCellEditorValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		if(PredmetiJTable.getInstance().getSelectedColumn() == 10) {        
+			PrikazPredmetaNaProfesoru p = new PrikazPredmetaNaProfesoru(); 
+			p.setVisible(true);
+			System.out.println("aaaaaaaaaa");
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Component getTableCellRendererComponent(JTable arg0, Object arg1, boolean arg2, boolean arg3, int arg4,
+			int arg5) {
+		JPanel p = new JPanel(new BorderLayout());  
+		p.add(this.renderB);
+		return p;
+	}
+
+	@Override
+	public Component getTableCellEditorComponent(JTable arg0, Object arg1, boolean arg2, int arg3, int arg4) {
+		JPanel p = new JPanel(new BorderLayout());
+		p.add(this.editB);
+		return p;
+		
+	}
+}
---
+++ SSluzba/src/view/PrikazPredmetaNaProfesoru.java
@@ -0,0 +1,79 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.Border;
+
+import model.BazaProfesori;
+import model.Predmet;
+import model.Profesor;
+
+public class PrikazPredmetaNaProfesoru extends JFrame{
+	public PrikazPredmetaNaProfesoru() {
+		setSize(400, 300);
+		setLocationRelativeTo(null);
+
+		setTitle("Spisak predmeta");
+		JPanel donjiPanel = new JPanel(new FlowLayout());
+		donjiPanel.setBackground(Color.DARK_GRAY);
+		donjiPanel.setPreferredSize(new Dimension(100,23));
+		DefaultListModel<String> tx = new DefaultListModel<String>(); 
+
+		Profesor st = new Profesor(BazaProfesori.getInstance().getRow(ProfesoriJTable.getInstance().getSelektovanRed()));
+		int i = 0;
+		for(Predmet temp : st.getPredmeti()) {
+			tx.add(i++,temp.getSifra_predmeta());
+		}
+		JList<String> lista = new JList<String>(tx);
+		lista.setPreferredSize(new Dimension(200,300));
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setViewportView(lista);
+		scrollPane.createVerticalScrollBar();
+		Border emptyBorder = BorderFactory.createEmptyBorder(20, 60, 100, 60);
+		scrollPane.setBorder(emptyBorder);
+
+	
+		
+		JPanel nazad = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		JButton btnNazad = new JButton("Nazad");
+		
+		
+		btnNazad.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				setVisible(false);
+				dispose();
+				
+			}
+		});
+		
+	
+		nazad.add(btnNazad);
+
+		
+
+		
+		Box struktura = Box.createVerticalBox();
+		struktura.add(nazad);
+		struktura.add(donjiPanel);
+		struktura.setPreferredSize(new Dimension(50,50));
+		this.add(scrollPane,BorderLayout.CENTER);
+		this.add(struktura,BorderLayout.SOUTH);
+		
+		setVisible(true);
+	}
+}
